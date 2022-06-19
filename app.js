74985cebf18401b362768943f9366e15c1002c4a
emerbautista <emer.bautistamendoza@gmail.com>
2022-06-18 19:41:23 -0500

    variable env

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
+const PORT = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
     console.log('Peticion recibida')
     res.status(200).send('Hola mundo')
 })
 
-app.listen(4000, () => {
-    console.log('Servidor escuchando')
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`)
 })
