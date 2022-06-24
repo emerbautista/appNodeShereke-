0dd79d9ad8010f39476617dc09649e3f9224379e
emerbautista <emer.bautistamendoza@gmail.com>
2022-06-24 11:56:19 -0500

    ApiFActuracion_FRE

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 require('dotenv').config()
 
 const express = require('express')
+const path = require('path')
 const app = express()
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res, next) => {
     console.log('Peticion recibida')
-    res.status(200).send('Hola mundo')
+    res.status(200).sendfile('index.html', {root: __dirname})
+    next()
 })
 
 app.listen(PORT, () => {
