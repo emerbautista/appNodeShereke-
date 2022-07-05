76e5e84f4e9ee4abf562067376dcd220cd63d072
emerbautista <emer.bautistamendoza@gmail.com>
2022-07-05 18:23:53 -0500

    Nodejs_pokemon

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
+const axios = require('axios')
 
 mongoose.connect(
     `mongodb+srv://emerbautista:${process.env.MONGO_DB_PASS}@clusterappshereke.yxnnhjn.mongodb.net/stock-app?retryWrites=true&w=majority`
@@ -37,9 +38,45 @@ app.post('/api/v1/products', (req, res) => {
     .catch((err) => console.log(err))
 })
 
-const PORT = process.env.PORT
+app.get('/', (req, res, next) => {
+    
+    const pokeapi = 'https://pokeapi.co/api/v2/pokemon'
+
+    axios(`${pokeapi}/charmander`)
+    .then((axiosResponse) => {
+        const pokemon = axiosResponse.data
+        console.log({pokemon})
+        const html = `
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>App de productos</title>
+            <link rel="stylesheet" href="style.css">
+            <script src="index.js" defer></script>
+        </head>
+        <body>
+            <h1>Primera app de productos</h1>
+            <a href="about.html">Sobre Emer</a>
+            <div class="form-container">
+                <input type="text" id="productName" placeholder="Nombre del producto">        
+                <input type="number" id="productPrice" placeholder="Precio del producto">        
+                <button>Crear producto</button>
+                <h2>Detalle de productos</h2>
+                <h3>${pokemon.name}</h3>
+                <img src="${pokemon.sprites.front_default}" alt="Esta es una imagen del pokemon ${pokemon.name}">
+                <span>#${pokemon.id}</span>     
+            </div>
+        </body>`
+        res.send(html)
+    })
+
+    next()
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT
 
 app.get('/', (req, res, next) => {
     console.log('Peticion recibida')
--- public/index.js
+++ public/index.js
@@ -2,6 +2,25 @@ const inputName = document.querySelector('#productName')
 const inputPrice = document.querySelector('#productPrice')
 
 const button = document.querySelector('button')
+const pokeapi = 'https://pokeapi.co/api/v2/pokemon'
+
+fetch(`${pokeapi}/charmander`)
+    .then((res) => {
+        return res.json()
+    }).then(pokemon => {
+        console.log({pokemon})
+        const html =  `
+        <h3>${pokemon.name}</h3>
+        <img src="${pokemon.sprites.front_default}" alt="Esta es una imagen del pokemon ${pokemon.name}">
+        <span>#${pokemon.id}</span>`
+
+        const div = document.createElement('div')
+        div.classList.add('poke-card')
+        div.innerHTML = html
+
+        document.querySelector('body').appendChild(div)
+    })
+    .catch()
 
 button.addEventListener('click', (e) => {
     const name = inputName.value
